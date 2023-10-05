e8dd303ecdb7dfed86a1802136b6562ca7eda06a
Boin Olivia Lee <121141971+Inhabitti@users.noreply.github.com>
2023-10-05 12:23:54 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,16 +1,12 @@
 // 네비게이션 메뉴 
 const menuIcon = document.getElementById('menu-icon');
-const closeButton = document.getElementById('close-button');
 const navMenu = document.querySelector('.nav-menu');
 
 menuIcon.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
 
-// 닫기 버튼 
-closeButton.addEventListener('click', () => {
-  navMenu.classList.remove('show');
-});
+// 닫기 
 
 document.addEventListener('click', (event) => {
   if (!navMenu.contains(event.target) && !menuIcon.contains(event.target)) {
